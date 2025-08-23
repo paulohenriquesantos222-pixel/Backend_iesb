4da7bea6c556f9d55cc7e7c8a41a08b0ed05f020
paulohenriquesantos222-pixel <paulohenriquesantos222@gmail.com>
2025-08-22 21:22:10 -0300

    ok

---
+++ praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,18 @@
+console.log("Meu primeiro projeto!!")
+
+console.log("Hello")
+
+let nome = 'Paulo'
+
+let sobrenome = 'Henrique'
+
+let idade = '25'
+
+console.log("Olá "+ nome +" "+ sobrenome)
+console.log("Sua idade é " + idade)
+
+if ( idade >=18){
+    console.log("Você é maior de idade!!!")
+} else{
+    console.log("Você é menor de idade!!!")
+}
